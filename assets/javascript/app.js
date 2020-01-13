ca64d24a7f4a0c7897d31a5516f458fcaa723a9c
Brandon Williams <brando5393@gmail.com>
2020-01-13 12:49:49 -0600

    started coding js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,23 @@
+const authKey = "";
+// Place your API key in the line above between the quotes
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ userSearchTerm +"&api-key=" + authKey;
+var userSearchTerm = "web development";
+var totalResults = 0;
+var startYear = 0;
+var endYear = 0;
+var articleCounter = 0;
+
+function runSearch(queryURL, totalResults){
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        Console.log(response);
+    })
+}
 $(document).ready(function(){
     $("#search-box").focus();
+    $("#search-btn").on("click", function(){
+        runSearch();
+    })
 });
