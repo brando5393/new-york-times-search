68d765ac05c945c7abbedfa8b0c72e79e91a13ea
Brandon Williams <brando5393@gmail.com>
2020-01-13 13:28:41 -0600

    started working on for loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 const authKey = "";
 // Place your API key in the line above between the quotes
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ userSearchTerm +"&api-key=" + authKey;
-var userSearchTerm = "web development";
+var userSearchTerm = "";
 var totalResults = 0;
 var startYear = 0;
 var endYear = 0;
@@ -13,11 +13,16 @@ function runSearch(queryURL, totalResults){
         method: "GET"
     }).then(function(response){
         Console.log(response);
+        for(i = 0; i < totalResults; i ++){
+            totalResults ++;
+            var articleDiv = $("<div>").attr('id', 'article-' + articleCounter + '-container');
+            $("#results").append(articleDiv);
+        }
     })
 }
 $(document).ready(function(){
     $("#search-box").focus();
     $("#search-btn").on("click", function(){
-        runSearch();
+        runSearch(queryURL, totalResults);
     })
 });
